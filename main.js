cd247458fe80524b38bb3abc223157de593fe947
Lucas Estefano <lucccasestefano_@hotmail.com>
2023-02-23 01:45:12 -0300

    Iniciado HTML plataforma de filmes

---
+++ main.js
@@ -0,0 +1,10 @@
+function localizaFilme(){
+    let localizaFilme = prompt("Informe o nome do filme que quer localizar: ")
+    alert("Filme não localizado!")
+
+    return localizaFilme
+}
+
+
+
+let localizarFilme = localizaFilme()
