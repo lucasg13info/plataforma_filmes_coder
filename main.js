436b98d772bc258a946708d4a9cd53e2aa4709ea
Lucas Estefano <lucccasestefano_@hotmail.com>
2023-02-23 01:53:27 -0300

    Incluido o objeto filme

--- main.js
+++ main.js
@@ -8,3 +8,17 @@ function localizaFilme(){
 
 
 let localizarFilme = localizaFilme()
+
+
+
+
+let filme = {
+    nome: 'Sherek 2',
+    generos: ['Filmes para a familia',  'Filmes baseados em livros', 'Filmes para toda a familia'],
+    categorias: 'Kids',
+    duracao: '1h 32',
+    indicativo: 'Livre para todos',
+    elenco: ['Mike Myes', 'Eddie Murphy', 'Cameron Diaz'],
+    resumo: 'O simpatico ogro e seu amor voltam para a terra natal dela para dizerem aos seus pais que se casaram, mas o Príncipe Encantado não fica nada feliz.'
+}
+    
