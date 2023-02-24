884e29b5dc15903b3d6ada0eb85ccc9562a39cd1
Pedro Lucas Carolino Felix <pedro.carolino.develop@gmail.com>
2023-02-24 15:57:40 -0300

    testanto o git

---
+++ main.js
@@ -0,0 +1,24 @@
+function localizaFilme(){
+    let localizaFilme = prompt("Informe o nome do filme que quer localizar: ")
+    alert("Filme não localizado!")
+
+    return localizaFilme
+}
+
+
+
+let localizarFilme = localizaFilme()
+
+
+
+
+let filmes = {
+    nome: 'Sherek 2',
+    generos: ['Filmes para a familia',  'Filmes baseados em livros', 'Filmes para toda a familia'],
+    categorias: 'Kids',
+    duracao: '1h 32',
+    indicativo: 'Livre para todos',
+    elenco: ['Mike Myes', 'Eddie Murphy', 'Cameron Diaz'],
+    resumo: 'O simpatico ogro e seu amor voltam para a terra natal dela para dizerem aos seus pais que se casaram, mas o Príncipe Encantado não fica nada feliz.'
+}
+    
